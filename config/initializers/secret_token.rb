5e9805842b543f432047d8331e3248b72c13a844
Zachary Sarette <zacharywsarette@gmail.com>
2011-05-15 10:18:54 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FocusSrs::Application.config.secret_token = 'fa5cdb92e3d0a5d8cf23d461eea7a3f77ff51ecf4755791383bae3b2fb9a8aee94585bc7b9a3332b9ce8722dd56d5c432d8bac0a838abcfe23f6845544a4514d'
